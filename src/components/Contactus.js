788d22a47793e23bdb676143e844b69948e2cfa6
Mohit Bagdi <92544103+edito0@users.noreply.github.com>
2022-10-12 00:07:42 +0530

    updated

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -6,15 +6,30 @@ import './css/contact.css' ;
 
  
 function Contactus() {
+
+  window.addEventListener('scroll', () => {
+    const contact = document.querySelector('.contact');
+    const section = document.querySelectorAll('.forminput');
+    const contentHeight = contact.getBoundingClientRect().top;
+
+    const screenPosition = window.innerHeight;
+
+    if (contentHeight < screenPosition) { 
+        section.forEach(e => { 
+            e.classList.add('forminput__active'); 
+        })
+    }
+});
+
   return (
     <>
       <div className='contact' id='contact'>
         <form action="">
-            <input type="text" name="" id=""  placeholder='Enter Your Name'/>
-            <input type="email" name="" id=""  placeholder='Enter Your Email'/>
-            <textarea name="" id="" cols="30" rows="10"  placeholder='message '></textarea>
-            <input type="submit" name="" id="" /> 
-        </form>
+            <input type="text" name="" id=""  placeholder='Enter Your Name' className='forminput'/>
+            <input type="email" name="" id=""  placeholder='Enter Your Email' className='forminput'/>
+            <textarea name="" id="" cols="30" rows="10"  placeholder='message '  className='forminput' ></textarea>
+            <input type="submit" name="" id=""  className='forminput' /> 
+        </form> 
 
         
 
